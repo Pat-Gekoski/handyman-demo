3e62a411e31a9fc534a7ebf7c94ed56568ab4e94
Patric Gekoski <=>
2025-01-27 19:27:28 -0500

    Initial commit

---
+++ app/(drawer)/_layout.tsx
@@ -0,0 +1,26 @@
+import { View, Text } from 'react-native'
+import { Drawer } from 'expo-router/drawer'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+
+const Layout = () => {
+	return (
+		<GestureHandlerRootView>
+			<Drawer screenOptions={{ drawerHideStatusBarOnOpen: true, drawerActiveTintColor: '#F2A310', headerTintColor: '#000' }}>
+				<Drawer.Screen
+					name='index'
+					options={{
+						title: 'Manage Locations',
+					}}
+				/>
+				<Drawer.Screen
+					name='location'
+					options={{
+						title: 'Location',
+					}}
+				/>
+			</Drawer>
+		</GestureHandlerRootView>
+	)
+}
+
+export default Layout
